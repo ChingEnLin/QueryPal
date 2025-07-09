ae6748244c8f5fb3cc9f0a98427d0c84b30148a5
CELin <lin@virtonomy.io>
2025-07-09 13:01:52 +0200

    feat: Replace ReactJson with JsonView from react-json-view-lite for enhanced JSON visualization

--- frontend/components/JsonCrackViewer.tsx
+++ frontend/components/JsonCrackViewer.tsx
@@ -1,22 +1,22 @@
+
 import React from 'react';
-import ReactJson from 'react-json-view';
+import { JsonView, darkStyles } from 'react-json-view-lite';
+import 'react-json-view-lite/dist/index.css';
 
 interface JsonCrackViewerProps {
   data: any;
 }
 
+
 const JsonCrackViewer: React.FC<JsonCrackViewerProps> = ({ data }) => {
   return (
     <div className="h-full w-full rounded-lg overflow-auto bg-gray-900 p-2">
-      <ReactJson
-        src={data}
-        theme="monokai"
-        iconStyle="circle"
+      <JsonView
+        data={data}
+        style={darkStyles}
+        enableClipboard
         displayDataTypes={false}
-        enableClipboard={true}
         collapsed={2}
-        name={false}
-        style={{ fontSize: '1rem', background: 'transparent' }}
       />
     </div>
   );
